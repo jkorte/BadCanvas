0025e4545a6f55c956338957a40b73a5e2e55630
jkorte <jessicakorte@gmail.com>
2024-09-29 15:01:19 +1000

    added connected dots drawing mode

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const buttons = {
     reset: document.querySelector("#reset"),
     smooth: document.querySelector("#smooth"),
     dots: document.querySelector("#dots"),
+    connected: document.querySelector("#connected"),
 }
 
 const canvas = document.querySelector("#my_canvas");
@@ -29,18 +30,21 @@ canvas.addEventListener("mouseup",change_draw_mode);
 canvas.addEventListener("mousemove",move_brush)
 canvas.addEventListener("touchstart",touch_draw);
 canvas.addEventListener("touchmove",touch_draw);
-canvas.addEventListener("touchend",()=>{
-    draw_mode = false;
-});
+canvas.addEventListener("touchend",change_draw_mode);
 
 buttons.reset.addEventListener("click",reset);
 buttons.dots.addEventListener("click",dot_mode);
 buttons.smooth.addEventListener("click",smooth_mode);
+buttons.connected.addEventListener("click", connected_mode)
 
 window.requestAnimationFrame(draw);
 
 function reset() {
     context.reset();
+}
+
+function connected_mode() {
+    draw_style = "connected"
 }
 
 function dot_mode() {
@@ -62,7 +66,16 @@ function draw() {
             context.beginPath();
             context.arc(brush.x, brush.y, size, 0, 2 * Math.PI);
             context.fill();
-        }        
+        } else if (draw_style === "connected") {
+            context.beginPath();
+            context.arc(brush.x, brush.y, size, 0, 2 * Math.PI);
+            context.fill();
+
+            context.beginPath();
+            context.moveTo(last.x, last.y);
+            context.lineTo(brush.x, brush.y);
+            context.stroke();
+        }
     }
 
     window.requestAnimationFrame(draw);
@@ -83,8 +96,12 @@ function move_brush(event) {
 }
 
 function touch_draw(event) {
+    event.preventDefault();
+
     draw_mode = true;
     console.log(event);
+    last.x = brush.x;
+    last.y = brush.y;
     brush.x = event.touches[0].clientX;
     brush.y = event.touches[0].clientY;
 }
